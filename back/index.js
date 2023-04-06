ae2ab80021501f12a46a1d3cad7f231873038f3d
Joaquin Lerer <joacolerer@gmail.com>
2023-04-06 18:53:48 -0300

    preparing for deploy

--- back/index.js
+++ back/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -105,7 +108,7 @@ app.put('/api/notes/:id', (request, response) =>{
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
